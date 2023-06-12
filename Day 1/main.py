954c47d21e90b9f92ecdfc4ed59180b336e316cc
Tommy Klapheke <tommyklapheke@yahoo.com>
2023-06-11 23:07:43 -0400

    Created a simple input program that takes user input from the city they grew up in and their pet's name to create a unique band name.

---
+++ Day 1/main.py
@@ -0,0 +1,11 @@
+# Band Name Generator is a simple input program that creates a unique band name
+print("Welcome to the Band Name Generator!!")
+
+# Input statements for city and pet name from the user
+city = input("What's the name of the city you grew up in? ")
+print(city)
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+
+# Prints your your band name
+print(f"And your new band name is..... {city} {pet_name}!!")
